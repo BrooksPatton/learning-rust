afdd183bb21ff0d3278170e57a8ee88ad1fbac35
Brooks Patton <brooks@brookzerker.com>
2018-06-27 08:02:01 -0600

    Converted to use a struct in the library

--- chapter12/minigrep/src/lib.rs
+++ chapter12/minigrep/src/lib.rs
@@ -1,30 +1,41 @@
-pub mod parse {
-  pub fn input<'a>(input: &'a Vec<String>) -> Result<(&'a str, &'a str), String> {
-    if input.len() == 3 {
-      Ok((&input[1], &input[2]))
-    } else {
-      Err(format!("Not the right amount of arguments, expected 2, got {}", input.len() - 1))
+pub mod grep {
+  pub struct Config {
+    pub query: String,
+    pub filename: String,
+  }
+
+  impl Config {
+    pub fn new(input: Vec<String>) -> Result<Config, String> {
+      if input.len() == 3 {
+        Ok(Config{
+          query: input[1].clone(),
+          filename: input[2].clone(),
+        })
+      } else {
+        Err(format!("Not the right amount of arguments, expected 2, got {}", input.len() - 1))
+      }
     }
   }
 }
 
 #[cfg(test)]
 mod tests {
-  use super::parse;
+  use super::*;
 
   #[test]
   fn parse_std_in() {
-      let std_in: Vec<String> = vec!["hello/world", "our_query", "the_filename"];
-      let config = parse::input(&std_in).unwrap();
+      let std_in: Vec<String> = vec![String::from("hello/world"), String::from("our_query"), String::from("the_filename")];
+
+      let config = grep::Config::new(std_in).unwrap();
 
       assert_eq!(config.query, "our_query");
       assert_eq!(config.filename, "the_filename");
   }
 
   #[test]
-  fn parse_input_should_error_when_not_correct_number_of_arguments() {
-    let std_in = vec!["hello/world".to_string()];
-    match parse::input(&std_in) {
+  fn config_should_error_when_not_correct_number_of_arguments() {
+    let std_in = vec![String::from("hello/world")];
+    match grep::Config::new(std_in) {
       Err(e) => assert_eq!(e, "Not the right amount of arguments, expected 2, got 0"),
       Ok(_) => panic!("We should not be ok"),
     }
--- chapter12/minigrep/src/main.rs
+++ chapter12/minigrep/src/main.rs
@@ -1,19 +1,19 @@
 extern crate minigrep;
-
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
-use minigrep::parse;
+use minigrep::grep;
 
-fn main() {
+fn main() -> Result<(), String> {
     let args: Vec<_> = env::args().collect();
+    let config = grep::Config::new(args)?;
 
-    let (query, filename) = parse::input(&args).unwrap();
-
-    let mut file = File::open(filename).expect("Error opening the file");
+    let mut file = File::open(&config.filename).expect("Error opening the file");
     let mut file_content = String::new();
     file.read_to_string(&mut file_content).expect("Error reading the file");
 
-    println!("We are searching for {} in {}", query, filename);
+    println!("We are searching for {} in {}", config.query, config.filename);
     println!("The file contents: {}", file_content);
+
+    Ok(())
 }
