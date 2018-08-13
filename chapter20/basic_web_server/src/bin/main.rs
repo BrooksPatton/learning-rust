0b325ff9cb486dcef1d4d4cbdce2686213586326
Brooks Patton <brooks@brookzerker.com>
2018-08-13 10:46:03 -0600

    server now takes more than 2 requests before shutting down

--- chapter20/basic_web_server/src/bin/main.rs
+++ chapter20/basic_web_server/src/bin/main.rs
@@ -13,7 +13,7 @@ fn main() {
     let listener = TcpListener::bind(address).unwrap();
     let thread_pool = ThreadPool::new(4);
 
-    for stream in listener.incoming().take(2) {
+    for stream in listener.incoming() {
         let stream = stream.unwrap();
 
         thread_pool.execute(|| handle_connection(stream));
